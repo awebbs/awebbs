0b62cb95967472bf18d2355f28d21aa2d5cafb7c
awebbs <57878642+awebbs@users.noreply.github.com>
2022-05-30 18:45:35 -0700

    Completed Weather Observation Station 6 HackerRank
    10 points

---
+++ SQL/WeatherObservationStation6.sql
@@ -0,0 +1,12 @@
+/*
+Query the list of CITY names that start with the vowels: (a, e, i, o, or u) from
+station. Your result cannot contain duplicates.
+*/
+
+select distinct city
+from station
+where city like ('a%')
+or city like ('e%')
+or city like ('i%')
+or city like ('o%')
+or city like ('u%');
