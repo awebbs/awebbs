b2754633406f0a3cd5cfa48610e65f6eb7dc3980
awebbs <57878642+awebbs@users.noreply.github.com>
2022-05-30 18:49:40 -0700

    Update and rename WeatherObservationStation6.sql to WeatherObservationStation6And7.sql
    Added Challenge 7 Query

--- SQL/WeatherObservationStation6And7.sql
+++ SQL/WeatherObservationStation6And7.sql
@@ -10,3 +10,15 @@ or city like ('e%')
 or city like ('i%')
 or city like ('o%')
 or city like ('u%');
+
+/*
+Query the list of CITY names that end with the vowels: (a, e, i, o, or u) from
+station. Your result cannot contain duplicates.
+*/
+select distinct city
+from station
+where city like ('%a')
+or city like ('%e')
+or city like ('%i')
+or city like ('%o')
+or city like ('%u');
