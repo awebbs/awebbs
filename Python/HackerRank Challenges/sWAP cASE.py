db99d09d7853dc802f3e48a08b0dc5c7e7cbae04
awebbs <57878642+awebbs@users.noreply.github.com>
2022-06-02 11:49:12 -0700

    Completed HackerRank Challenge sWAP cASE
    Language: Python3

---
+++ Python/HackerRank Challenges/sWAP cASE.py
@@ -0,0 +1,13 @@
+'''You are given a string and your task is to swap cases. In other words, 
+convert all lowercase letters to uppercase letters and vice versa.'''
+
+def swap_case(s):
+    newString = ''
+    for i in s:
+        if i.islower():
+            newString = newString + i.upper()          
+        if i.isupper():
+            newString = newString + i.lower()
+        if i.isalpha() == False:
+            newString = newString + i
+    return newString
