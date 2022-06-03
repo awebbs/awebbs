62dd207181706c07967060eb69c103a280af36eb
awebbs <57878642+awebbs@users.noreply.github.com>
2022-06-02 17:44:02 -0700

    Completed Splitting & Joining a string Challenge
    Python3

--- Python/HackerRank Challenges/MutateAString.sql
+++ Python/HackerRank Challenges/MutateAString.sql
@@ -7,3 +7,14 @@ def mutate_string(string, position, character):
     string = ''.join(li)
     
     return string
+    
+/*
+Prompt: You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+*/
+
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    line = "-".join(line)
+    
+    return line
