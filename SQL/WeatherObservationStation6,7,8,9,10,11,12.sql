bcec4337369d2a53f294b65021affe00ab6c5b35
awebbs <57878642+awebbs@users.noreply.github.com>
2022-06-03 16:24:33 -0700

    Added in WO 13

--- SQL/WeatherObservationStation6,7,8,9,10,11,12.sql
+++ SQL/WeatherObservationStation6,7,8,9,10,11,12.sql
@@ -56,3 +56,11 @@ select distinct city
 from station
 where city not rlike '^[aeiou]'
 and city not rlike '[aeiou]$;
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
+Truncate your answer to decimal places.
+*/
+select round(sum(lat_n), 4)
+from station
+where lat_n > 38.7880 and lat_n < 137.2345;
