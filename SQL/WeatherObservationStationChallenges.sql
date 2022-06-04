49c5224dae74ee699d9d035d4c82168e69eb673b
awebbs <57878642+awebbs@users.noreply.github.com>
2022-06-04 13:31:38 -0700

    Added WOS 18 completed challenge

--- SQL/WeatherObservationStationChallenges.sql
+++ SQL/WeatherObservationStationChallenges.sql
@@ -80,3 +80,15 @@ Round your answer to 4 decimal places.
 select round(min(lat_n), 4)
 from station
 where lat_n > 38.7880;
+
+/*
+Consider P1(a, b) and P2(c, d) to be two points on a 2D plane.
+a: happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b: happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c: happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d: happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points and and round it to a scale of 4 decimal places.
+*/
+select round((max(lat_n) - min(lat_n)) + (max(long_w) - min(long_w)), 4)
+from station;
