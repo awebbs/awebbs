04d62f775cd51ddab397d51c9886fdee61e40ff4
awebbs <57878642+awebbs@users.noreply.github.com>
2022-06-04 11:43:29 -0700

    Added WOS 16

--- SQL/WeatherObservationStation6,7,8,9,10,11,12, 13.sql
+++ SQL/WeatherObservationStation6,7,8,9,10,11,12, 13.sql
@@ -74,9 +74,9 @@ from station
 where lat_n < 137.2345;
 
 /*
-Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) from STATION that is less than 137.2345. 
-Truncate your answer to 4 decimal places.
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7880. 
+Round your answer to 4 decimal places.
 */
-select round(long_w, 4)
+select round(min(lat_n), 4)
 from station
-where max(lat_n) < 137.2345);
+where lat_n > 38.7880;
